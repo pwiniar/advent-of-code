ab3e24c87f4102f1aed9e18004fa5a412dd0ac25
pwiniar <winiarskipawel@gmail.com>
2022-12-28 14:48:18 +0100

    day 6 part 1 and 2

--- src/main/java/aoc/year2022/days/Day6.java
+++ src/main/java/aoc/year2022/days/Day6.java
@@ -3,32 +3,53 @@ package aoc.year2022.days;
 import aoc.year2022.Year2022;
 import org.assertj.core.api.Assertions;
 
-import java.util.Arrays;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class Day6 extends Year2022 {
     public Day6() {
         super(6);
     }
 
+
+    /*
+     * sequence of four characters that are all different
+     *
+     * */
+
     @Override
     public Object example1() {
-        return null;
+        var input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+        int res = findMarker(4, input);
+
+        Assertions.assertThat(res).isEqualTo(7);
+        return res;
     }
 
     @Override
     public Object part1() {
-        return null;
+        return findMarker(4, day());
     }
 
     @Override
     public Object example2() {
-        return null;
+        var input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+        int res = findMarker(14, input);
+
+        Assertions.assertThat(res).isEqualTo(19);
+        return res;
     }
 
     @Override
     public Object part2() {
-        return null;
+        return findMarker(14, day());
+    }
+
+    private int findMarker(int size, String input) {
+        return IntStream.range(0, input.length())
+                .filter(i -> input.substring(i, i + size)
+                        .chars().distinct().count() == size)
+                .findFirst().getAsInt() + size;
     }
 }
